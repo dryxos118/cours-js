7a6c64ba87af50b8de44167f71fe1c1beced1ad7
alexis <alexishu59134@gmail.com>
2022-10-31 11:18:56 +0100

    cours js

--- 03-projets-DOM/09-video/app.js
+++ 03-projets-DOM/09-video/app.js
@@ -5,3 +5,19 @@ console.log(gif);
 window.addEventListener("load", function () {
   gif.classList.add("hide-preloader");
 });
+
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+console.log(btn);
+console.log(video);
+
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.add("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
